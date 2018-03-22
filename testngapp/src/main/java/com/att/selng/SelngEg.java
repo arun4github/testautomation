ab8110c369abafaed5f59cb8cee3e2900f88defd
arunrajrajendran <arunraj.rajendran@hotmail.com>
2018-03-22 12:02:53 -0700

    updates

--- testngapp/src/main/java/com/att/selng/SelngEg.java
+++ testngapp/src/main/java/com/att/selng/SelngEg.java
@@ -1,5 +1,7 @@
 package com.att.selng;
 
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -8,45 +10,46 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SelngEg {
-	
+
 	WebDriver driver;
-	
-	public SelngEg()
-	{
+	String subWindowHandler;
+	public SelngEg() {
 		System.setProperty("webdriver.chrome.driver", "/Users/arunrajrajendran/Applications/chromedriver");
-		 driver = new ChromeDriver();
+		driver = new ChromeDriver();
 	}
-	
-	public String getPageUrl(String url)
-	{
-		
-		String loadedUrl="";
+
+	public String getPageUrl(String url) {
+
+		String loadedUrl = "";
 		driver.get(url);
-		loadedUrl=driver.getCurrentUrl();
+		loadedUrl = driver.getCurrentUrl();
 
 		return loadedUrl;
 	}
-	
-	
-	
-	public boolean searchProduct(String url,String keyword) throws InterruptedException
-	{
+
+	public boolean searchProduct(String url, String keyword) throws InterruptedException {
 		driver.get(url);
+		driver.manage().deleteAllCookies();
+		driver.manage().window().fullscreen();
 		Thread.sleep(2000);
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys(keyword);
-		
-		driver.findElement(By.xpath(".//*[@id=\"nav-search\"]/form/div[2]/div/input")).click();
-		
-//		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//		System.out.println(driver.findElement(By.xpath(".//*[@id=\\\"result_1\\\"]/div/div/div/div[2]/div[1]/div[1]/a")).getText());
-//		//driver.findElement(By.xpath(".//*[@id=\"result_1\"]/div/div/div/div[2]/div[1]/div[1]/a")).click();
 
+		driver.findElement(By.xpath(".//*[@id=\"nav-search\"]/form/div[2]/div/input")).click();
+
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		System.out.println(
+				driver.findElement(By.xpath("//*[@id=\"result_0\"]/div/div/div/div[2]/div[1]/div[1]/a")).getText());
+		driver.findElement(By.xpath("//*[@id=\"result_0\"]/div/div/div/div[2]/div[1]/div[1]/a")).click();
+		driver.findElement(By.id("add-to-cart-button")).click();
+		driver.findElement(By.id("add-to-cart-button")).click();
+		
+	
 		return driver.getCurrentUrl().contains(keyword);
 	}
-	
-	public void quitDriver()
-	{
-		
+
+	public void quitDriver() {
+
 		driver.quit();
 	}
 }
--- testngapp/src/test/java/testng/TestNGsample.java
+++ testngapp/src/test/java/testng/TestNGsample.java
@@ -4,13 +4,27 @@ import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
+import com.att.selng.RestClient;
 import com.att.selng.SelngEg;
+import com.att.selng.Userdata;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import junit.framework.Assert;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
 import static org.testng.Assert.assertEquals;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 
@@ -18,20 +32,27 @@ import org.testng.annotations.AfterTest;
 public class TestNGsample {
 	
 	 SelngEg selNg;
-	
-	@BeforeTest
+	  String getEPurl=null;
+	  RestClient restClient;
+	String finalUrl;
+	@BeforeTest(groups= {"seltest"})
 	public void beforeTest()
 	{
 		 selNg = new SelngEg();
+		  
+		  getEPurl="https://reqres.in";
+		  restClient = new RestClient();
 	}
 	@Test
   public void loadURLTest() {
+		
 	  String finalurl=selNg.getPageUrl("https://www.amazon.com");
 	  assertEquals(finalurl, "https://www.amazon.com/");
   }
-	@Test
+	@Test(dependsOnMethods= {"loadURLTest"})
   public void clickedContentTrue() {
 	  try {
+		  
 		boolean value=selNg.searchProduct("https://www.amazon.com","macbookPro");
 		assertEquals(value, true);
 	} catch (InterruptedException e) {
@@ -40,12 +61,55 @@ public class TestNGsample {
 	}
 
   }
+	
+	//@Test(groups= {"apitest"})
+	  public void getStatusCodeTestPass() throws ClientProtocolException, IOException {
+		  finalUrl=getEPurl+"/api/users/2";
+		  CloseableHttpResponse getResponse=restClient.getRequest(finalUrl);
+		  int statusCode=getResponse.getStatusLine().getStatusCode();
+		  Assert.assertEquals(200, statusCode);
+		  finalUrl=null;
+		 
+	  }
+	  
+	//@Test(groups= {"apitest"})
+	  public void getStatusCodeTestFailure() throws ClientProtocolException, IOException {
+		  finalUrl=getEPurl+"/api/users/245535";
+		  CloseableHttpResponse getResponse=restClient.getRequest(finalUrl);
+		  int statusCode=getResponse.getStatusLine().getStatusCode();
+		  
+		  Assert.assertEquals(404, statusCode);
+		  finalUrl=null;
+	  }
+	  
+	  
+	  
+//	@Test(groups= {"apitest"})
+	  public void post_user_data_test_pass() throws JsonGenerationException, JsonMappingException, IOException
+	  {
+		  finalUrl= getEPurl+"/api/users";
+		HashMap<String,String> userMap= new HashMap<String,String>();
+		userMap.put("Content-Type", "application/json");
+		//jackson API:
+				ObjectMapper mapper = new ObjectMapper();
+				Userdata userdata = new Userdata();
+				userdata.setName("black panther");
+				userdata.setJob("Guard humans");
+				mapper.writeValue(new File("/Users/arunrajrajendran/git/testngapp/src/main/java/com/att/selng/userdata.json"), userdata);
+				String usersJsonString = mapper.writeValueAsString(userdata);
+				System.out.println(usersJsonString);
+				CloseableHttpResponse postResponse=restClient.postRequest(finalUrl, usersJsonString, userMap);
+				int statusCode=postResponse.getStatusLine().getStatusCode();
+				System.out.println("statusCode--"+statusCode);
+				assertEquals(statusCode, 201);
+		
+	  }
   
   
-  @AfterTest
+  /*@AfterTest
   public void afterMethod() throws InterruptedException {
-	  Thread.sleep(10000);
+	  Thread.sleep(3000);
   selNg.quitDriver();
-  }
+  }*/
 
 }
